c9dce6dc95280022c203acc24e7ed3d162645948
Madasu Deepika <91784120+MadasuDeepika@users.noreply.github.com>
2023-03-27 07:44:24 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hello, I am Madasu Deepika. Currently, Pursuing my M.Tech integrated CSE specialization in Business Analytics at VIT Chennai. <br> <br></span> In a world where 'data is power' is the current saying, I am continuously honing my skills to keep up with the ever-evolving digital transformations and providing solutions and insights by potentially making the fullest use of the data presented to me. I am always keen on problem solving and building challenging applications that are robust, resilient and secure. A team player who likes to help and get helped.<br><br> I am passionate about pursuing a challenging and fulfilling career with a legitimate company that acknowledges my true potential while enhancing my analytical as well as technical expertise."
+      ],
+      typeSpeed: 10,
+      contentType: 'html'
+    });
+  }, 1000);
